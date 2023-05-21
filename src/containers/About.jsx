72efa18001002892ba72713c5572d554056ba1b9
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-21 12:17:40 +0300

    add and style the about page. create a different button component for home page and style the button components. modify some other styles.

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-import { TopSection } from '../components'
-import { aboutPage } from '../constants/data'
+import { Button, TopSection } from '../components'
+import { aboutPageData } from '../constants/data'
+import { about } from '../constants/images'
+import './containers.css'
 
 const About = () => (
     <div>
-        <TopSection topHeading={aboutPage.topHeading} />
+        <TopSection topHeading={aboutPageData.topHeading} />
+        <div className="container pt-5 pb-3 mt-5 lh-lg">
+            <div className="row justify-content-between align-items-center">
+                <div className="col-11 col-md-5 mx-auto">
+                    <h1 className='about__heading'> {aboutPageData.secondHeading} </h1>
+                    <p className='text-secondary mb-4'> { aboutPageData.content } </p>
+                    <Button about={aboutPageData.button} />
+                </div>
+                <img src={about} alt="about us" className='about__img col-11 col-md-6 pt-5 pb-3 mt-5 mx-auto' />
+            </div>
+        </div>
     </div>
 )
 
--- src/containers/home/HomeTopSection.jsx
+++ src/containers/home/HomeTopSection.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { homePage } from '../../constants/data'
-import { Button } from '../../components'
+import { homePageData } from '../../constants/data'
+import HomeButton from './HomeButton'
 import './home.css'
 
 const HomeTopSection = () => (
     <div className="top-section text-white text-center mb-5">
         <div className="container">
-            <h1 className='top-section__heading fw-bolder mb-5'> { homePage.topHeading } </h1>
-            <p className="top-section__p mx-auto mb-5"> { homePage.introText }</p>
-            <Button className='top-section__button' about={ homePage.topButton } />
+            <h1 className='top-section__heading fw-bolder mb-5'> { homePageData.topHeading } </h1>
+            <p className="top-section__p mx-auto mb-5"> { homePageData.introText }</p>
+            <HomeButton className='top-section__button' about={ homePageData.topButton } />
         </div>
     </div>
 )
